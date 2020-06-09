5580ebb58856e2aac82ad0c3c4628239c584ab49
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-06-09 12:19:50 +0200

    Add transaction. Delete transaction with button x

--- script.js
+++ script.js
@@ -31,15 +31,42 @@ const fakeTransactions = [{
 
 let transactions = fakeTransactions;
 
-// Add transactions
-const addTransaction = (transaction) => {
+// Get new id
+const getID = () => {
+    return Math.floor(Math.random() * 10000000);
+}
+
+// Add transaction
+const addTransaction = (e) => {
+    e.preventDefault();
+
+    // Create new transaction object
+    const transaction = {
+        id: getID(),
+        text: addText.value,
+        amount: +addAmount.value
+    }
+
+    // Push object to transactions array and update DOM
+    transactions.push(transaction);
+    addTransactionDOM(transaction);
+    updateValues();
+
+    // Clear inputs
+    addText.value = "";
+    addAmount.value = "";
+}
+
+
+// Add transactions to DOM
+const addTransactionDOM = (transaction) => {
     // Income or expense
     const sign = transaction.amount < 0 ? 'expense' : 'income';
 
     // Add item to list
     const item = document.createElement('li');
     item.classList.add(sign === 'expense' ? 'expense' : 'income');
-    item.innerHTML = `${transaction.text} <span>${transaction.amount}</span><button class="delete-btn" id="delete-btn">x</button>`
+    item.innerHTML = `${transaction.text} <span>${transaction.amount}</span><button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>`
 
     list.appendChild(item);
 }
@@ -48,24 +75,32 @@ const addTransaction = (transaction) => {
 const updateValues = () => {
     const amounts = transactions.map(transaction => transaction.amount);
     // Balance
-    const balanceValue = amounts.reduce((acc, item) => acc + item).toFixed(2);
+    const balanceValue = amounts
+        .reduce((acc, item) => (acc + item), 0)
+        .toFixed(2);
 
     // Total expense
     const expenseValue = amounts
         .filter(item => item < 0)
-        .reduce((acc, item) => acc + item)
+        .reduce((acc, item) => (acc + item), 0)
         .toFixed(2);
 
     // Total income
     const incomeValue = amounts
         .filter(item => item > 0)
-        .reduce((acc, item) => acc + item)
+        .reduce((acc, item) => (acc + item), 0)
         .toFixed(2);
 
     // Update DOM 
     balance.textContent = `$${balanceValue}`;
     income.textContent = `$${incomeValue}`;
-    expense.textContent = `$${Math.abs(expenseValue)}`;
+    expense.textContent = `$${Math.abs(expenseValue).toFixed(2)}`;
+}
+
+// Remove transaction
+const removeTransaction = (id) => {
+    transactions = transactions.filter(transaction => transaction.id !== id);
+    init();
 }
 
 // Init app
@@ -73,9 +108,12 @@ const init = () => {
     list.innerHTML = ""
 
     transactions.forEach(item => {
-        addTransaction(item);
+        addTransactionDOM(item);
     })
     updateValues();
 }
 
 init();
+
+// Event listeners
+form.addEventListener('submit', addTransaction);
